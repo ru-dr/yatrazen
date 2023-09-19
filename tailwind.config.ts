fa80afc0a2205a47f729d99dd09f40386c19eecc
Lazycoder26 <Rudra26_2004@outlook.com>
2023-09-19 12:04:21 +0530

    navbar done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,15 @@ const config: Config = {
         'space': ['Space Mono', 'sans-serif']
       },
     },
+    screens: {
+      
+      'xs': '320px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl' : '1280px',
+      '2xl': '1536px',
+    }
   },
   plugins: [],
 }

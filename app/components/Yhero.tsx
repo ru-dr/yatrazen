68933be996b4c2587970882566b5d68745ee4aa9
Lazycoder26 <Rudra26_2004@outlook.com>
2023-09-20 09:46:54 +0530

    at risk

--- app/components/Yhero.tsx
+++ app/components/Yhero.tsx
@@ -1,9 +1,26 @@
 export default function Yhero() {
   return (
-    <div className="hero flex bg-[#beffc5] h-96 w-full rounded-2xl my-4">
-      <div></div>
-      <div></div>
-      <div></div>
+    <div className="hero flex flex-col bg-[#beffc5] h-96 w-full justify-center rounded-2xl my-4 gap-y-4">
+      <div className="flex w-1/2 justify-around items-center mx-auto">
+        <div className="xxs:hidden xs:hidden sm:block w-96 h-14 sm:w-6/12 md:w-4/12 lg:4/12 xxs: xs: sm: md: lg: xl: xxl: 2xl: 2xxl: 3xl: ">
+          <img
+            src="https://images.pexels.com/photos/2618804/pexels-photo-2618804.jpeg?auto=compress&cs=tinysrgb&w=1600"
+            alt=""
+            className="w-full h-full object-cover rounded-full xxs: xs: sm: md: lg: xl: xxl: 2xl: 2xxl: 3xl:"
+          />
+        </div>
+        <p className="st-text flex justify-center font-semibold xxs:text-2xl xs:text-3xl sm:text-4xl md:text-5xl lg:text-5xl xl:text-5xl xxl:text-6xl 2xl:text-6xl 2xxl:text-6xl 3xl:text-6xl">
+          Make your journey
+        </p>
+      </div>
+      <div>
+        <p></p>
+        <div></div>
+      </div>
+      <div>
+        <div></div>
+        <p></p>
+      </div>
     </div>
   );
 }

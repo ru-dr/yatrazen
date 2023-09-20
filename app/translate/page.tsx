ae62d9c2af01ac3918d9e94d8d80bcaefe30d545
Vedant Pandey <vedantpandey434@gmail.com>
2023-09-20 22:08:59 +0530

    kuch bhi commit kar dia

--- app/translate/page.tsx
+++ app/translate/page.tsx
@@ -1,6 +1,5 @@
 import Ynav from "../components/Ynav";
-import Ytranslate from "../components/Ytranslate";
-
+import Ytranslate from "./components/Ytranslate";
 export default function Translate() {
   return (
     <main className="mx-6 my-2">

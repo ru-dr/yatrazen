963433482317dbcc70b07f55a4e21c0b0db8fc25
Lazycoder26 <Rudra26_2004@outlook.com>
2023-09-22 13:58:14 +0530

    weather done

--- app/weather/components/Yweather.tsx
+++ app/weather/components/Yweather.tsx
@@ -1,126 +1,244 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Spinner } from "@nextui-org/react";
+import {
+  IoMdSunny,
+  IoMdRainy,
+  IoMdCloudy,
+  IoMdSnow,
+  IoMdThunderstorm,
+  IoMdSearch,
+} from "react-icons/io";
+import {
+  BsCloudHaze2Fill,
+  BsCloudDrizzleFill,
+  BsEye,
+  BsWater,
+  BsWind,
+  BsThermometer,
+} from "react-icons/bs";
+import { TbTemperatureCelsius } from "react-icons/tb";
 
-// Define a type for the weather data (adjust this as needed)
+// b2319354382f61fb8056a07f2d6cb2bc
+const APIkey = "b2319354382f61fb8056a07f2d6cb2bc";
+
 interface WeatherData {
+  name: string;
+  sys: {
+    country: string;
+  };
+  dt: number;
   main: {
-    temp: number;
+    temp: any;
+    feels_like: any;
+    humidity: number;
+  };
+  visibility: number;
+  wind: {
+    speed: any;
   };
   weather: {
+    main: string;
     description: string;
   }[];
-  name: string;
-  // Add other properties as needed
 }
 
-function getCurrentDate() {
-    const currentDate = new Date();
-    const options = { month: "long" as const };
-    const monthName = currentDate.toLocaleString("en-US", options);
-    const date = new Date().getDate() + ", " + monthName;
-    return date;
-}
+export default function Home() {
+  const [data, setData] = useState<WeatherData | null>(null);
+  const [location, setLocation] = useState<string>("Mumbai"); // default location
+  const [inputValue, setInputValue] = useState<string>(""); // input value
+  const [animate, setAnimate] = useState<boolean>(false); // animation
+  const [loading, setLoading] = useState<boolean>(false); // loading
 
+  // This function sets the value of the input field to the value of the event.
+  const handleInputValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
 
-
-const Yweather: React.FC = () => {
-  const date = getCurrentDate();
-  const [weatherData, setWeatherData] = useState<null | WeatherData>(null);
-  const [city, setCity] = useState<string>("");
-
-  async function fetchData(cityName: string) {
-    try {
-      const response = await fetch(
-        "http://localhost:3000/api/weather?address=" + cityName
-      );
-      const jsonData = (await response.json()).data;
-      setWeatherData(jsonData);
-    } catch (error) {
-      console.error("Error fetching data:", error);
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (inputValue !== "") {
+      setLocation(inputValue);
     }
-  }
 
-  async function fetchDataByCoordinates(latitude: number, longitude: number) {
-    try {
-      const response = await fetch(
-        `http://localhost:3000/api/weather?lat=${latitude}&lon=${longitude}`
-      );
-      const jsonData = (await response.json()).data;
-      setWeatherData(jsonData);
-    } catch (error) {
-      console.error("Error fetching data:", error);
+    const input = document.querySelector("input");
+
+    if (input && input.value === "") {
+      setAnimate(true);
+      setTimeout(() => {
+        setAnimate(false);
+      }, 500);
     }
-  }
+    if (input) {
+      input.value = "";
+    }
+  };
 
+  // fetch the data
   useEffect(() => {
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(
-        (position: GeolocationPosition) => {
-          const { latitude, longitude } = position.coords;
-          fetchDataByCoordinates(latitude, longitude);
-        },
-        (error) => {
-          console.error("Error getting geolocation:", error);
-        }
-      );
-    }
-  }, []);
+    // set the loading to true
+    setLoading(true);
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${APIkey}`;
+
+    axios.get(url).then((response) => {
+      setTimeout(() => {
+        // set the data after 1.5 seconds
+        setData(response.data);
+        setLoading(true);
+      }, 1500);
+      setData(response.data);
+    });
+  }, [location]);
+
+  // console.log(data);
+  // if data is false show loading
+  if (!data) {
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <div>
+          
+        </div>
+      </div>
+    );
+  }
+
+  let icon;
+  switch (data.weather[0].main) {
+    case "Clear":
+      icon = <IoMdSunny />;
+      break;
+    case "Clouds":
+      icon = <IoMdCloudy />;
+      break;
+    case "Rain":
+      icon = <IoMdRainy />;
+      break;
+    case "Snow":
+      icon = <IoMdSnow />;
+      break;
+    case "Thunderstorm":
+      icon = <IoMdThunderstorm />;
+      break;
+    case "Haze":
+      icon = <BsCloudHaze2Fill />;
+      break;
+    case "Drizzle":
+      icon = <BsCloudDrizzleFill />;
+      break;
+    default:
+      icon = <IoMdSunny />;
+  }
+
+  const date = new Date(data.dt * 1000);
 
   return (
-    <main className="">
-      <article className="">
-        <form
-          className=""
-          onSubmit={(e: React.FormEvent) => {
-            e.preventDefault();
-            fetchData(city);
-          }}
-        >
+    <div>
+      <form
+        onSubmit={(e) => handleSubmit(e)}
+        className={`${
+          animate ? "animate-spin" : "animate-none"
+        } h-16 bg-black/30 w-full max-w-[450px] rounded-full backdrop-blur-[32px] mb-8`}
+      >
+        <div className="h-full relative flex items-center justify-between p-2">
           <input
-            className=""
-            placeholder="Enter city name"
+            onChange={(e) => handleInputValue(e)}
+            className="flex-1 bg-transparent outline-none placeholder:text-white text-white text-[15px] font-light pl-6 h-full"
             type="text"
-            id="cityName"
-            name="cityName"
-            value={city}
-            onChange={(e) => setCity(e.target.value)}
+            placeholder="Search by City or Country"
           />
-          <button className="" type="submit">
-            Search
+          <button
+            type="submit"
+            className="bg-[#00bfff] w-20 h-12 rounded-full flex items-center justify-center hover:bg-cyan-700"
+          >
+            <IoMdSearch className="text-2xl text-white" />
           </button>
-        </form>
-        {weatherData && weatherData.weather && weatherData.weather[0] ? (
-          <>
-            <div className="">
-              <div className="">
-                {weatherData?.weather[0]?.description === "rain" ||
-                weatherData?.weather[0]?.description === "fog" ? (
-                  <i className=""></i>
-                ) : (
-                  <i className=""></i>
-                )}
+        </div>
+      </form>
+      {/* card */}
+      <div className="w-full max-w-[450px] bg-black/60 min-h-[584px] text-white backdrop-blur-[32px] rounded-[16px] py-8 px-6">
+        <div>
+          {/* card top */}
+          <div className="flex items-center gap-x-5 rounded-lg">
+            <div className="text-[87px]">{icon}</div>
+            <div className="flex flex-col">
+              <div className="text-2xl font-bold">
+                {data.name}, {data.sys.country}
               </div>
-              <div className="">
-                <div className="">
-                  <span>
-                    {(weatherData?.main?.temp - 273.5).toFixed(2) +
-                      String.fromCharCode(176)}
-                  </span>
+              <div className="text-sm">{date.toLocaleDateString()}</div>
+            </div>
+          </div>
+          {/* card body */}
+          <div className="my-20">
+            <div className="flex justify-center items-center">
+              <div className="text-[144px] leading-none font-light">
+                {parseInt(data.main.temp)}
+              </div>
+              {/* celcius icon */}
+              <div className="text-4xl">
+                <TbTemperatureCelsius />
+              </div>
+            </div>
+            {/* weather discription*/}
+            <div className="capitalize text-center">
+              {data.weather[0].description}
+            </div>
+          </div>
+          {/* card bottom */}
+          <div className="max-w-[378px] mx-auto flex flex-col gap-y-6">
+            <div className="flex justify-between">
+              <div className="flex items-center gap-x-2">
+                {/* icon */}
+                <div className="text-[20px]">
+                  <BsEye />
                 </div>
                 <div>
-                  {weatherData?.weather[0]?.description?.toUpperCase()}
+                  Visibility{" "}
+                  <span className="ml-2">{data.visibility / 1000} Km</span>
+                </div>
+              </div>
+              <div className="flex items-center gap-x-2">
+                {/* icon */}
+                <div className="text-[20px]">
+                  <BsThermometer />
+                </div>
+                <div className="flex">
+                  Feels Like{" "}
+                  <div className="flex ml-2">
+                    {parseInt(data.main.feels_like)}
+                    <TbTemperatureCelsius />
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="">{weatherData?.name}</div>
-            <div className="">{date}</div>
-          </>
-        ) : (
-          <div className="">Loading...</div>
-        )}
-      </article>
-    </main>
+            <div className="flex justify-between">
+              <div className="flex items-center gap-x-2">
+                {/* icon */}
+                <div className="text-[20px]">
+                  <BsWater />
+                </div>
+                <div>
+                  Humidity
+                  <span className="ml-2">{data.main.humidity} %</span>
+                </div>
+              </div>
+              <div className="flex items-center gap-x-2">
+                {/* icon */}
+                <div className="text-[20px]">
+                  <BsWind />
+                </div>
+                <div>
+                  Wind
+                  <span className="ml-2">
+                    {`${(data.wind.speed * 3.6).toString()} Km/H`}
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
-};
-
-export default Yweather;
+}

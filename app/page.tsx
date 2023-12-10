eee2420c58422515e7af524581af03a77e0185c4
Lazycoder26 <Rudra26_2004@outlook.com>
2023-12-10 22:25:28 +0530

    Refactor Home component and update project message

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,59 @@
+// import Yhero from "./components/Yhero";
+// import Ynav from "./components/Ynav";
+// import Yplaces from "./components/Yplaces";
+
+// export default function Home() {
+//   return (
+//     <main className="mx-6">
+//         <Ynav />
+//       <div className="my-4 -z-0">
+//         <Yhero />
+//       </div>
+//       <div className="-z-0">
+//         <Yplaces />
+//       </div>
+//     </main>
+//   );
+// }
+
 import React from "react";
-import Yhero from "./components/Yhero";
-import Ynav from "./components/Ynav";
-import Yplaces from "./components/Yplaces";
+
+const MobileAppScreenshotURL =
+  "https://ucarecdn.com/1c53f2b7-4a42-4e6a-b4c0-ad298bbba937/-/scale_crop/838x1264/-/format/auto/-/quality/best/";
 
 export default function Home() {
   return (
-    <div className="container mx-auto p-6">
-      {/* <Ynav />
-      <section className="my-4">
-        <Yhero />
-      </section>
-      <section>
-        <Yplaces />
-      </section> */}
-
-      {/* Project archived message */}
-      <section className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-4xl font-bold mb-4">
-          v2 is No Longer in Development
+    <div className="container mx-auto p-8 md:p-16 flex flex-col md:flex-row items-center justify-between min-h-screen">
+      {/* Left Section - Project Archived Message */}
+      <section className="md:w-1/2 md:mr-8 flex flex-col items-center justify-center mb-8 md:mb-0">
+        <h1 className="text-5xl font-bold mb-4 text-center">
+          Project Update: v2 Archived
         </h1>
-        <p className="text-xl text-center mb-8">
+        <p className="text-lg md:text-xl mb-4 text-center">
           The web version (v2) of the project is no longer actively maintained.
           Development has shifted to the mobile version (v3) for iOS and Android
-          devices. Head over to{" "}
+          devices.
+        </p>
+        <p className="text-lg md:text-xl mb-8 text-center">
+          Check out the{" "}
           <a
             href="https://github.com/ru-dr/v3"
             className="text-blue-500 hover:text-blue-700"
           >
-            GitHub
+            v3 development on GitHub
           </a>{" "}
-          to see and contribute to the development of v3 for mobile devices.
+          for mobile devices.
         </p>
+      </section>
+
+      {/* Right Section - Mobile App Screenshot */}
+      <section className="md:w-1/2 flex items-center justify-center">
+        {/* Displaying the mobile app screenshot from the provided URL */}
+        <img
+          src={MobileAppScreenshotURL}
+          alt="Mobile App Screenshot"
+          className="mb-8 h-auto w-96 sm:w-64 lg:w-80 xl:w-96"
+        />
       </section>
     </div>
   );

88d50d05a5feb306e8a4a080734c532537e89743
Lazycoder26 <Rudra26_2004@outlook.com>
2023-12-10 22:09:09 +0530

    Add project archived message to Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,37 @@
+import React from "react";
 import Yhero from "./components/Yhero";
 import Ynav from "./components/Ynav";
 import Yplaces from "./components/Yplaces";
 
 export default function Home() {
   return (
-    <main className="mx-6">
-        <Ynav />
-      <div className="my-4 -z-0">
+    <div className="container mx-auto p-6">
+      {/* <Ynav />
+      <section className="my-4">
         <Yhero />
-      </div>
-      <div className="-z-0">
+      </section>
+      <section>
         <Yplaces />
-      </div>
-    </main>
+      </section> */}
+
+      {/* Project archived message */}
+      <section className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold mb-4">
+          v2 is No Longer in Development
+        </h1>
+        <p className="text-xl text-center mb-8">
+          The web version (v2) of the project is no longer actively maintained.
+          Development has shifted to the mobile version (v3) for iOS and Android
+          devices. Head over to{" "}
+          <a
+            href="https://github.com/ru-dr/v3"
+            className="text-blue-500 hover:text-blue-700"
+          >
+            GitHub
+          </a>{" "}
+          to see and contribute to the development of v3 for mobile devices.
+        </p>
+      </section>
+    </div>
   );
 }

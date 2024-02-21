4f5403aa30c19eac5bf4a5399df678f68c296990
Rudra Patel <77568653+ru-dr@users.noreply.github.com>
2024-02-21 15:30:44 +0530

    Added Newer Version in FB

--- app/components/Feedback.jsx
+++ app/components/Feedback.jsx
@@ -9,7 +9,7 @@ const Feedback = () => {
       </div>
       <p className="release">
         Share your feedback on{" "}
-        <span className="font-bold "> ( v4.0.0b ) </span>{" "}
+        <span className="font-bold "> ( v4.0.5b ) </span>{" "}
         <span>
           <Link
             href="/feedback"
